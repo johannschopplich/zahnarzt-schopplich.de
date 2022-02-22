f76bd6d0d875d4215456d114f2e70c5da980013e
Johann Schopplich <mail@johannschopplich.com>
2022-02-22 20:27:26 +0100

    chore: update layout

--- src/components/NoticeHoliday.vue
+++ src/components/NoticeHoliday.vue
@@ -34,30 +34,28 @@
 
       <div class="columns">
         <div class="column">
-          <div class="border border-contrast-lowest p-lg rounded-sm">
-            <h4 class="mb-1">
-              Zahnarztpraxis Lesser & Rudolf (vormals Dr. Kokott)
-            </h4>
-            <p class="text-size-sm leading-tight mb-lg">
-              Dr.-Friedrich-Wolf-Straße 2<br />
-              07545 Gera
-            </p>
-            <div class="columns items-center gap-sm">
-              <div class="column-12 sm:column-narrow">
-                <a
-                  href="tel:03658001160"
-                  class="due-button-primary !text-size-sm !p-2"
-                >
-                  <CarbonPhoneFilled class="mr-1" aria-hidden="true" />
-                  03 65 / 800 11 60
-                </a>
-              </div>
-              <div class="column-auto">
-                <p class="text-rose-600 text-size-sm leading-tight">
-                  <strong>Achtung</strong>: Eine vorherige telefonische
-                  Anmeldung ist unbedingt erforderlich!
-                </p>
-              </div>
+          <h4 class="mb-1">
+            Zahnarztpraxis Lesser & Rudolf (vormals Dr. Kokott)
+          </h4>
+          <p class="text-size-sm leading-tight mb-lg">
+            Dr.-Friedrich-Wolf-Straße 2<br />
+            07545 Gera
+          </p>
+          <div class="columns items-center gap-sm">
+            <div class="column-12 sm:column-narrow">
+              <a
+                href="tel:03658001160"
+                class="due-button-primary !text-size-sm !p-2"
+              >
+                <CarbonPhoneFilled class="mr-1" aria-hidden="true" />
+                03 65 / 800 11 60
+              </a>
+            </div>
+            <div class="column-auto">
+              <p class="text-rose-600 text-size-sm leading-tight">
+                <strong>Achtung</strong>: Eine vorherige telefonische Anmeldung
+                ist unbedingt erforderlich!
+              </p>
             </div>
           </div>
         </div>
