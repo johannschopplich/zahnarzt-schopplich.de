431f70da0aaaf98a0207f4df3a48abcfac3ec823
Johann Schopplich <mail@johannschopplich.com>
2022-04-12 08:41:12 +0200

    chore: upgrade dependencies

--- src/logic/dark-color-scheme-check.ts
+++ src/logic/dark-color-scheme-check.ts
@@ -1,9 +1,5 @@
 (() => {
-  // const prefersDark = matchMedia("(prefers-color-scheme: dark)").matches;
-  // const setting = localStorage.getItem("vueuse-color-scheme") || "auto";
-  // if (setting === "dark" || (prefersDark && setting !== "light")) {
-  //   document.documentElement.classList.toggle("dark", true);
-  // }
-
   document.documentElement.dataset.theme = "light";
 })();
+
+export {};
